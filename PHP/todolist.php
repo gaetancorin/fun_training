e6295f99f2d75d2888f4d62c0917aea5a20a0137
gaetancorin <gaetancorin@gmail.com>
2022-12-01 12:00:00 +0100

    Add todolist html

--- PHP/todolist.php
+++ PHP/todolist.php
@@ -1,4 +1,8 @@
 <?php
+// Pour lancer, ecrire dans le terminal:
+// php -S localhost:8000
+// Puis aller sur
+// http://localhost:8000/todolist.php
 $file = 'taches.json';
 
 $taches = [];
@@ -30,3 +34,72 @@ if (isset($_GET['toggle'])) {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mini To-Do List PHP</title>
+    <style>
+        body {
+            font-family: Arial;
+            background: #f4f4f4;
+            display: flex;
+            justify-content: center;
+            padding-top: 50px;
+        }
+        .todolist {
+            background: white;
+            padding: 20px 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px #ccc;
+            width: 400px;
+        }
+        input, button {
+            padding: 10px;
+            margin: 5px 0;
+            font-size: 16px;
+        }
+        ul {
+            list-style: none;
+            padding-left: 0;
+        }
+        li {
+            margin: 8px 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        li.fait {
+            text-decoration: line-through;
+            color: gray;
+        }
+        a {
+            margin-left: 10px;
+            text-decoration: none;
+            color: red;
+        }
+    </style>
+</head>
+<body>
+<div class="todolist">
+    <h2>📝 Mini To-Do List</h2>
+    <form method="POST">
+        <input type="text" name="nouvelle_tache" placeholder="Nouvelle tâche" required>
+        <button type="submit">Ajouter</button>
+    </form>
+
+    <ul>
+        <?php foreach ($taches as $index => $tache): ?>
+            <li class="<?= $tache['faite'] ? 'fait' : '' ?>">
+                <span><?= htmlspecialchars($tache['texte']) ?></span>
+                <div>
+                    <a href="?toggle=<?= $index ?>">✔️</a>
+                    <a href="?supprimer=<?= $index ?>">❌</a>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+</body>
+</html>
