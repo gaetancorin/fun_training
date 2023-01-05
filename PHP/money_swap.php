10de86127fb7c3c22dfcb7334c0cb846c4b7a6d4
gaetancorin <gaetancorin@gmail.com>
2023-01-05 12:00:00 +0100

    add interface to money_convertisseur project

--- PHP/money_swap.php
+++ PHP/money_swap.php
@@ -26,3 +26,76 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>💱 MoneySwap - Convertisseur de devises</title>
+<style>
+body {
+    font-family: Arial, sans-serif;
+    background: #e0f7fa;
+    display: flex;
+    justify-content: center;
+    padding-top: 50px;
+}
+.money-swap {
+    background: white;
+    padding: 25px 35px;
+    border-radius: 12px;
+    box-shadow: 0 4px 15px rgba(0,0,0,0.2);
+    width: 400px;
+}
+h2 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+input, select, button {
+    width: 100%;
+    padding: 12px;
+    margin: 6px 0;
+    font-size: 16px;
+    border-radius: 6px;
+    border: 1px solid #ccc;
+}
+button {
+    background: #00796b;
+    color: white;
+    border: none;
+    cursor: pointer;
+    font-weight: bold;
+}
+button:hover {
+    background: #004d40;
+}
+.resultat {
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 18px;
+    text-align: center;
+}
+</style>
+</head>
+<body>
+<div class="money-swap">
+    <h2>💱 MoneySwap</h2>
+    <form method="POST">
+        <input type="number" step="any" name="montant" placeholder="Montant" required>
+        <select name="from" required>
+            <?php foreach ($devises as $dev => $rate) echo "<option value='$dev'>$dev</option>"; ?>
+        </select>
+        <select name="to" required>
+            <?php foreach ($devises as $dev => $rate) echo "<option value='$dev'>$dev</option>"; ?>
+        </select>
+        <button type="submit">Convertir</button>
+    </form>
+
+    <?php if ($resultat !== ''): ?>
+        <div class="resultat">
+            <?= htmlspecialchars($montant) ?> <?= $_POST['from'] ?> = <?= $resultat ?> <?= $_POST['to'] ?>
+        </div>
+    <?php endif; ?>
+</div>
+</body>
+</html>
