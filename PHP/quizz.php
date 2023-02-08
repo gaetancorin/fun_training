2361de81854c049eef1995cc554dcaa9e8221de6
gaetancorin <gaetancorin@gmail.com>
2023-02-08 12:00:00 +0100

    quizz interface

--- PHP/quizz.php
+++ PHP/quizz.php
@@ -1,7 +1,8 @@
 <?php
 // Pour lancer, écrire dans le terminal :
 // php -S localhost:8000
-// Puis aller sur http://localhost:8000/mini_quiz.php
+// Puis aller sur:
+// http://localhost:8000/quizz.php
 
 session_start();
 
@@ -47,3 +48,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Mini Quiz PHP</title>
+<style>
+body { font-family: Arial; display: flex; justify-content: center; padding-top: 50px; background: #f0f0f0; }
+.quiz { background: white; padding: 20px 30px; border-radius: 10px; box-shadow: 0 0 10px #ccc; width: 500px; }
+button { padding: 10px 20px; margin-top: 10px; font-size: 16px; }
+</style>
+</head>
+<body>
+<div class="quiz">
+    <h2>📝 Mini Quiz</h2>
+
+    <?php if (!isset($finished) || !$finished): ?>
+        <?php $current = isset($nextQuestion) ? $nextQuestion : 0; ?>
+        <form method="POST">
+            <p><strong>Question <?= $current + 1 ?>:</strong> <?= $questions[$current]['question'] ?></p>
+            <?php foreach ($questions[$current]['options'] as $i => $opt): ?>
+                <label>
+                    <input type="radio" name="choice" value="<?= $i ?>" required>
+                    <?= $opt ?>
+                </label><br>
+            <?php endforeach; ?>
+            <input type="hidden" name="question" value="<?= $current ?>">
+            <button type="submit">Valider</button>
+        </form>
+        <?php if (isset($feedback)) echo "<p>$feedback</p>"; ?>
+    <?php else: ?>
+        <h3>🎉 Quiz terminé !</h3>
+        <p>Votre score : <?= $score ?> / <?= count($questions) ?></p>
+        <a href="mini_quiz.php"><button>Recommencer</button></a>
+    <?php endif; ?>
+</div>
+</body>
+</html>
