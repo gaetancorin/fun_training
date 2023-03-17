8a49733d603a88bf26b20d70096b15949c02d603
gaetancorin <gaetancorin@gmail.com>
2023-03-17 12:25:00 +0100

    improve survey

--- PHP/survey.php
+++ PHP/survey.php
@@ -31,3 +31,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['choix'])) {
 $totalVotes = array_sum($votes);
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>📊 Survey PHP</title>
+<style>
+body { font-family: Arial; background: #f0f0f0; display:flex; justify-content:center; padding-top:50px; }
+.container { background:white; padding:20px 30px; border-radius:10px; box-shadow:0 0 10px #ccc; width:500px; text-align:center;}
+button { padding:10px 20px; margin:5px; font-size:16px; cursor:pointer;}
+.message { margin:10px 0; font-weight:bold; color:green;}
+.bar { height:20px; background:#00796b; margin:5px 0; color:white; text-align:center; line-height:20px; border-radius:5px;}
+</style>
+</head>
+<body>
+<div class="container">
+<h2>📊 Survey : Quel est ton langage préféré ?</h2>
+
+<?php if($message !== ''): ?>
+<div class="message"><?= $message ?></div>
+<?php endif; ?>
+
+<?php if(!isset($_SESSION['a_vote']) || $_SESSION['a_vote'] === false): ?>
+<form method="POST">
+    <button type="submit" name="choix" value="PHP">PHP</button>
+    <button type="submit" name="choix" value="Python">Python</button>
+    <button type="submit" name="choix" value="JavaScript">JavaScript</button>
+    <button type="submit" name="choix" value="Autre">Autre</button>
+</form>
+<?php endif; ?>
+
+<h3>📊 Résultats :</h3>
+<?php foreach ($votes as $option => $count):
+    $percent = $totalVotes > 0 ? round($count / $totalVotes * 100) : 0;
+?>
+<div><?= $option ?> : <?= $count ?> vote<?= $count>1?'s':'' ?> (<?= $percent ?>%)</div>
+<div class="bar" style="width:<?= $percent ?>%"><?= $percent ?>%</div>
+<?php endforeach; ?>
+
+<p>Total votes : <?= $totalVotes ?></p>
+</div>
+</body>
+</html>
