1aeffc6273ab48439ecde8bd36546b11a31d04dc
gaetancorin <gaetancorin@gmail.com>
2023-05-10 12:25:00 +0200

    improve date display on meteo scrapper matplotlib

--- PYTHON/meteo_scrapper.py
+++ PYTHON/meteo_scrapper.py
@@ -5,12 +5,10 @@
 import requests
 import matplotlib.pyplot as plt
 from datetime import datetime
+import matplotlib.dates as mdates
 import sys
 
-def fetch_weather(lat, lon, hours=24):
-    """
-    Récupère les données météo via Open‑Meteo pour les prochaines heures.
-    """
+def fetch_weather(lat, lon, hours=48):
     url = (
         f"https://api.open-meteo.com/v1/forecast?"
         f"latitude={lat}&longitude={lon}"
@@ -25,26 +23,32 @@ def fetch_weather(lat, lon, hours=24):
     return resp.json()
 
 def plot_temperature(data):
-    """
-    Affiche le graphique des températures horaires.
-    """
     times = data["hourly"]["time"]
     temps = data["hourly"]["temperature_2m"]
-    # Conversion des temps ISO => datetime
+
     dates = [datetime.fromisoformat(t) for t in times]
 
-    plt.figure(figsize=(10,5))
-    plt.plot(dates, temps, marker="o", linestyle="-", color="orange")
+    # Filtrer pour ne pas surcharger l'axe X (une valeur toutes les 3 heures)
+    dates_filtered = dates[::3]
+    temps_filtered = temps[::3]
+
+    plt.figure(figsize=(12,5))
+    plt.plot(dates_filtered, temps_filtered, marker="o", linestyle="-", color="orange")
     plt.title("Température horaire")
     plt.xlabel("Date / Heure")
     plt.ylabel("Température (°C)")
+
+    # Formater l'axe X pour afficher jour/mois et heure
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m %Hh'))
+    plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
+
     plt.xticks(rotation=45)
     plt.grid(True)
     plt.tight_layout()
     plt.show()
 
 def main():
-    # Exemple : Paris (lat, lon)
+    # Exemple : Paris
     lat = 48.8566
     lon = 2.3522
     if len(sys.argv) == 3:
