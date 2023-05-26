ef4194e2d27b54f3b3147eda13f98dc31f21a00e
gaetancorin <gaetancorin@gmail.com>
2023-05-26 12:25:00 +0200

    electricity_conumption_scrapper for France, and display with matplotlib

---
+++ PYTHON/electricity_consumption_scrapper.py
@@ -0,0 +1,67 @@
+# electricity_consumption_scrapper.py
+# Nécessite : pip install requests pandas matplotlib
+
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta, timezone
+import matplotlib.dates as mdates
+
+def fetch_eco2mix_data():
+    """
+    Télécharge les données Eco2mix en open data (consommation et production électrique France).
+    """
+    url = "https://opendata.reseaux-energies.fr/explore/dataset/eco2mix-national-tr/download/?format=csv&timezone=Europe/Paris"
+    try:
+        df = pd.read_csv(
+            url,
+            sep=';',
+            usecols=[
+                'date_heure','consommation','nucleaire','eolien','solaire','hydraulique','fioul','gaz'
+            ],
+            parse_dates=['date_heure']
+        )
+        df.rename(columns={'date_heure':'datetime'}, inplace=True)
+        # Conversion en datetime avec fuseau horaire UTC pour homogénéiser
+        df['datetime'] = pd.to_datetime(df['datetime'], utc=True, errors='coerce')
+        return df.dropna(subset=['datetime'])
+    except Exception as e:
+        print("Erreur lors de la récupération :", e)
+        return None
+
+def plot_consumption_and_production(df, hours=48):
+    """
+    Affiche la consommation et les principales sources de production.
+    """
+    now = datetime.now(timezone.utc)  # -> datetime "aware" (avec fuseau horaire UTC)
+    df_recent = df[df['datetime'] >= now - timedelta(hours=hours)]
+
+    plt.figure(figsize=(12,6))
+    plt.plot(df_recent['datetime'], df_recent['consommation'], label="Consommation", color='black', linewidth=2)
+
+    plt.plot(df_recent['datetime'], df_recent['nucleaire'], label="Nucléaire", color='blue')
+    plt.plot(df_recent['datetime'], df_recent['eolien'], label="Éolien", color='green')
+    plt.plot(df_recent['datetime'], df_recent['solaire'], label="Solaire", color='orange')
+    plt.plot(df_recent['datetime'], df_recent['hydraulique'], label="Hydraulique", color='cyan')
+    plt.plot(df_recent['datetime'], df_recent['fioul'], label="Fioul", color='red')
+    plt.plot(df_recent['datetime'], df_recent['gaz'], label="Gaz", color='brown')
+
+    plt.title(f"Consommation et production électrique en France (dernières {hours} heures)")
+    plt.xlabel("Date / Heure (UTC)")
+    plt.ylabel("MW")
+    plt.legend()
+    plt.grid(True)
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m %Hh'))
+    plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    print("Récupération des données Eco2mix…")
+    df = fetch_eco2mix_data()
+    if df is not None and not df.empty:
+        plot_consumption_and_production(df, hours=48)
+
+if __name__ == "__main__":
+    main()
