7234bc1fd67fd2cbe2f3bb03c6b6c6a7e4945478
gaetancorin <gaetancorin@gmail.com>
2023-11-04 12:26:00 +0100

    improve analysis into picsum analysis

--- PYTHON/picsum_color_analyser.py
+++ PYTHON/picsum_color_analyser.py
@@ -2,31 +2,40 @@ import requests
 from io import BytesIO
 from PIL import Image
 from collections import Counter
+import matplotlib.pyplot as plt
 
-# URL de l'image (Picsum pour l'exemple)
+# URL de l'image
 url = "https://picsum.photos/200/300"
-
-# Récupérer l'image
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
-
-# Convertir en RGB si nécessaire
 img = img.convert("RGB")
 
-# Reduire la taille pour accélérer l'analyse
+# Réduction pour analyse rapide
 img_small = img.resize((50, 50))
-
-# Extraire toutes les couleurs
 pixels = list(img_small.getdata())
 
 # Compter les couleurs
 color_counts = Counter(pixels)
-
-# Afficher les 5 couleurs les plus fréquentes
 top_colors = color_counts.most_common(5)
-print("Top 5 couleurs dominantes (RGB) :")
-for color, count in top_colors:
-    print(color, ":", count, "pixels")
 
-# Optionnel : afficher l'image
-img.show()
+colors = [color for color, count in top_colors]
+counts = [count for color, count in top_colors]
+hex_colors = ['#%02x%02x%02x' % color for color in colors]
+
+# Affichage
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 8))
+
+# 1. Image originale
+ax1.imshow(img)
+ax1.axis('off')
+ax1.set_title("Image originale")
+
+# 2. Graphique des couleurs dominantes
+ax2.bar(range(len(top_colors)), counts, color=hex_colors)
+ax2.set_xticks(range(len(top_colors)))
+ax2.set_xticklabels(hex_colors)
+ax2.set_ylabel("Nombre de pixels")
+ax2.set_title("Top 5 couleurs dominantes")
+
+plt.tight_layout()
+plt.show()
