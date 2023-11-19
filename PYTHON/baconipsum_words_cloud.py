1f43ff8d37102fce4c67c1708a60208694f04db7
gaetancorin <gaetancorin@gmail.com>
2023-11-19 12:26:00 +0100

    Have most commun baconipsum words

--- PYTHON/baconipsum_words_cloud.py
+++ PYTHON/baconipsum_words_cloud.py
@@ -1,16 +1,22 @@
 import requests
+from collections import Counter
+import re
 
-# --- Paramètres ---
-N = 5  # nombre de paragraphes à récupérer
+N = 5
 
-# --- 1. Appel à l'API Bacon Ipsum ---
 url = f"https://baconipsum.com/api/?type=meat-and-filler&paras={N}"
 response = requests.get(url)
+paragraphs = response.json()
 
-if response.status_code == 200:
-    paragraphs = response.json()  # renvoie une liste de paragraphes
-    print(f"{len(paragraphs)} paragraphes récupérés :\n")
-    for i, para in enumerate(paragraphs, 1):
-        print(f"Paragraphe {i} : {para}\n")
-else:
-    print("Erreur lors de la récupération :", response.status_code)
+print(f"{len(paragraphs)} paragraphes récupérés :\n")
+for i, para in enumerate(paragraphs, 1):
+    print(f"Paragraphe {i} : {para}\n")
+
+text = " ".join(paragraphs).lower()
+words = re.findall(r'\b\w+\b', text)
+counter = Counter(words)
+most_common = counter.most_common(20)
+
+print("Top 20 mots les plus fréquents :")
+for word, count in most_common:
+    print(f"{word} : {count}")
