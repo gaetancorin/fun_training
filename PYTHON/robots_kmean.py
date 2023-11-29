74c00471557621035e8f2497ad6879108ec0a496
gaetancorin <gaetancorin@gmail.com>
2023-11-29 12:27:00 +0100

    create kmean clustering into robots pictures

--- PYTHON/robots_kmean.py
+++ PYTHON/robots_kmean.py
@@ -6,10 +6,13 @@ from PIL import Image
 import numpy as np
 import colorsys
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 NUM_IMAGES = 20
 OUTPUT_DIR = "robots"
 SIZE = (256, 256)
+NUM_CLUSTERS = 4
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -43,27 +46,35 @@ for i in range(1, NUM_IMAGES + 1):
 
 print(f"\nTerminé : {len(images)} images téléchargées et sauvegardées dans '{OUTPUT_DIR}'.")
 
-# === Étape 2 : Extraction des caractéristiques visuelles ===
+# Étape 2 : extraction des caractéristiques visuelles
 features = []
 
 for ident, img in zip(ids, images):
     arr = np.array(img) / 255.0
     mask = ~(np.all(arr == [0, 0, 0], axis=-1))
     arr = arr[mask]
-
     r, g, b = arr.mean(axis=0)
     hsv = np.array([colorsys.rgb_to_hsv(*px) for px in arr])
     h_mean, s_mean, v_mean = hsv.mean(axis=0)
+    features.append([r, g, b, s_mean, v_mean])
 
-    features.append({
-        "id": ident,
-        "mean_r": round(r * 255, 1),
-        "mean_g": round(g * 255, 1),
-        "mean_b": round(b * 255, 1),
-        "saturation": round(s_mean, 3),
-        "luminosity": round(v_mean, 3)
-    })
+X = np.array(features)
 
-df = pd.DataFrame(features)
-print("\nAperçu des caractéristiques extraites :")
-print(df.head())
+# Étape 3 : clustering KMeans
+kmeans = KMeans(n_clusters=NUM_CLUSTERS, random_state=42)
+labels = kmeans.fit_predict(X)
+
+# Visualisation des robots par cluster
+clustered = {i: [] for i in range(NUM_CLUSTERS)}
+for label, img, ident in zip(labels, images, ids):
+    clustered[label].append((ident, img))
+
+plt.figure(figsize=(NUM_CLUSTERS * 4, 8))
+for cluster_idx, items in clustered.items():
+    for i, (ident, img) in enumerate(items):
+        plt.subplot(NUM_CLUSTERS, max(len(items),1), cluster_idx * len(items) + i + 1)
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title(f"{ident}\nCluster {cluster_idx}")
+plt.tight_layout()
+plt.show()
