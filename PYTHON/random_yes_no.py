3280faabefa368519f6495b02ed0e54ca32fc20d
gaetancorin <gaetancorin@gmail.com>
2023-12-12 12:28:00 +0100

    animate meme into random_yes_no program

--- PYTHON/random_yes_no.py
+++ PYTHON/random_yes_no.py
@@ -3,24 +3,46 @@ from io import BytesIO
 from PIL import Image, ImageTk
 import tkinter as tk
 
+frames = []  # frames du GIF actuel
+anim_id = None  # identifiant de l'after pour annuler l'animation
+
+def animate(counter=0):
+    global anim_id
+    frame = frames[counter % len(frames)]
+    label_img.config(image=frame)
+    label_img.image = frame
+    anim_id = root.after(100, animate, counter+1)
 
 def get_yesno():
+    global frames, anim_id
+
+    # stoppe l'animation en cours
+    if anim_id:
+        root.after_cancel(anim_id)
+        anim_id = None
+
     try:
         response = requests.get("https://yesno.wtf/api")
         data = response.json()
         answer = data["answer"].capitalize()
         img_url = data["image"]
+
         img_resp = requests.get(img_url)
         img = Image.open(BytesIO(img_resp.content))
-        img = img.resize((300, 300))  # redimensionne le GIF pour l'affichage
-        img_tk = ImageTk.PhotoImage(img)
+
+        frames = []
+        try:
+            while True:
+                frame = img.copy().resize((300, 300))
+                frames.append(ImageTk.PhotoImage(frame))
+                img.seek(len(frames))
+        except EOFError:
+            pass
 
         label_text.config(text=f"Réponse : {answer}")
-        label_img.config(image=img_tk)
-        label_img.image = img_tk
+        animate()  # relance l'animation
     except Exception as e:
         label_text.config(text=f"Erreur : {e}")
-
 
 root = tk.Tk()
 root.title("Yes/No API 🎲")
