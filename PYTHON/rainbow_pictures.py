a3366ee893c4b42c0d447d585e0ea78f58205f97
gaetancorin <gaetancorin@gmail.com>
2023-12-29 12:28:00 +0100

    filters color into random colors

--- PYTHON/rainbow_pictures.py
+++ PYTHON/rainbow_pictures.py
@@ -3,14 +3,17 @@ import time
 import requests
 from PIL import Image
 from io import BytesIO
+import numpy as np
+import colorsys
 
-NUM_IMAGES = 20
+NUM_IMAGES = 30
 SIZE = 500  # image carrée 500x500
 OUTPUT_DIR = "picsum_images_square"
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 images = []
+dominant_colors = []
 
 for i in range(1, NUM_IMAGES + 1):
     url = f"https://picsum.photos/{SIZE}/{SIZE}?random={i}"
@@ -18,7 +21,6 @@ for i in range(1, NUM_IMAGES + 1):
         resp = requests.get(url)
         resp.raise_for_status()
         img = Image.open(BytesIO(resp.content)).convert("RGB")
-        # recadrage carré centré si nécessaire
         w, h = img.size
         min_edge = min(w, h)
         left = (w - min_edge) // 2
@@ -26,13 +28,21 @@ for i in range(1, NUM_IMAGES + 1):
         right = left + min_edge
         bottom = top + min_edge
         img = img.crop((left, top, right, bottom))
-        img = img.resize((SIZE, SIZE))  # assurer la taille finale
+        img = img.resize((SIZE, SIZE))
         filename = os.path.join(OUTPUT_DIR, f"image_{i}.png")
         img.save(filename)
         images.append(img)
-        print(f"[{i}/{NUM_IMAGES}] sauvegardé -> {filename}")
+
+        # --- Extraction couleur dominante ---
+        arr = np.array(img) / 255.0
+        mask = ~(np.all(arr == [0, 0, 0], axis=-1))  # ignore le noir
+        arr = arr[mask]
+        mean_rgb = arr.mean(axis=0)
+        dominant_colors.append(mean_rgb)
+
+        print(f"[{i}/{NUM_IMAGES}] sauvegardé -> {filename} | couleur dominante: {mean_rgb}")
     except Exception as e:
         print(f"[{i}/{NUM_IMAGES}] erreur :", e)
     time.sleep(0.2)
 
-print(f"\nTerminé : {len(images)} images carrées téléchargées dans '{OUTPUT_DIR}'.")
+print(f"\nTerminé : {len(images)} images carrées téléchargées avec couleur dominante extraite.")
