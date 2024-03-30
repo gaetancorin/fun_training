590aa1df2c25f54b83bb2b6ef3ff44aa9b096bc3
gaetancorin <gaetancorin@gmail.com>
2024-03-30 12:06:00 +0100

    fix problem on creating container in dag airflow

--- AIRFLOW/dags/create_docker_into_task.py
+++ AIRFLOW/dags/create_docker_into_task.py
@@ -15,18 +15,20 @@ with DAG(
         auto_remove='force',  # Remove container after execution
         mounts=[Mount(source="/tmp", target="/tmp", type="bind")], # Create space into host for creating container
         environment={"EXAMPLE": "Hello Docker"},  # Environment variables
-        # network_mode="bridge",  # Network mode
+        mount_tmp_dir=False,
+        do_xcom_push=False,
     )
     def docker_task():
         import os
         print("Running inside Docker container!")
         print(f"EXAMPLE env variable: {os.environ['EXAMPLE']}")
 
-        # You can run any Python code here
+        # Run any Python code here
         import subprocess
         subprocess.check_call(["pip", "install", "requests"])
         import requests
         resp = requests.get("https://api.github.com")
         print(f"GitHub API status: {resp.status_code}")
+        return resp.status_code
 
     docker_task()
