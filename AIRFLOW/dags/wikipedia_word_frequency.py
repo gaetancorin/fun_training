9d86db34aeeb5975e49b8986d772da181122be6f
gaetancorin <gaetancorin@gmail.com>
2024-06-06 12:06:00 +0200

    dag wikipedia count reccurency and create json file

--- AIRFLOW/dags/wikipedia_word_frequency.py
+++ AIRFLOW/dags/wikipedia_word_frequency.py
@@ -3,6 +3,8 @@ from airflow.decorators import task
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from collections import Counter
+import matplotlib.pyplot as plt
 import re
 import os
 import json
@@ -44,7 +46,20 @@ with DAG(
         print(f"✅ Scraping terminé, {len(all_words)} mots récupérés.")
         return words_path
 
+    @task()
+    def compute_word_counts(words_path: str):
+        with open(words_path) as f:
+            words = json.load(f)
+
+        counter = Counter(words)
+        most_common = counter.most_common(50)
+
+        counts_path = os.path.join(DATA_DIR, "wikipedia_word_counts.json")
+        with open(counts_path, "w") as f:
+            json.dump(most_common, f, indent=2)
+        print("✅ Comptage des mots terminé.")
+        return counts_path
 
 
     words_path = scrape_pages()
-
+    counts_path = compute_word_counts(words_path)
