70e5bf94f528d9503ef0b2d8ae9d7c1d9d22a683
gaetancorin <gaetancorin@gmail.com>
2024-07-12 12:06:00 +0200

    sucess craeate docker container into dag

--- AIRFLOW/dags/create_docker_into_task.py
+++ AIRFLOW/dags/create_docker_into_task.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.decorators import task
 from datetime import datetime
+from docker.types import Mount
 
 with DAG(
     dag_id="create_docker_into_task",
@@ -12,15 +13,18 @@ with DAG(
         docker_url="tcp://host.docker.internal:2375",  # Docker TCP host
         image="python:3.12-slim",  # Docker image to use
         auto_remove='force',  # Remove container after execution
-        mounts=[],  # Optional: mounts like volume mounts
+        mounts=[Mount(source="/tmp", target="/tmp", type="bind")], # Create space into host for creating container
         environment={"EXAMPLE": "Hello Docker"},  # Environment variables
-        network_mode="bridge",  # Network mode
+        # network_mode="bridge",  # Network mode
     )
     def docker_task():
         import os
         print("Running inside Docker container!")
         print(f"EXAMPLE env variable: {os.environ['EXAMPLE']}")
+
         # You can run any Python code here
+        import subprocess
+        subprocess.check_call(["pip", "install", "requests"])
         import requests
         resp = requests.get("https://api.github.com")
         print(f"GitHub API status: {resp.status_code}")
