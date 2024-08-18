e00310c0430981cd58501eadbe6f693698e7d863
gaetancorin <gaetancorin@gmail.com>
2024-08-18 12:07:00 +0200

    add warn and error into logs analysis spark

--- SPARK/V4_apache_spark/workspace/read_logs_container.py
+++ SPARK/V4_apache_spark/workspace/read_logs_container.py
@@ -3,13 +3,14 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("ReadLogs").master("local[*]").getOrCreate()
 sc = spark.sparkContext
 
-# Lire les fichiers logs directement depuis le container master
 rdd = sc.textFile("/opt/spark/logs/*.out")  # ou *.log selon tes fichiers
 
-# Exemple : compter les occurrences du mot "ERROR"
-info_count = rdd.filter(lambda line: "INFO" in line).count()
-print(f"Nombre de lignes avec INFO : {info_count}")
+error_count = rdd.filter(lambda line: "ERROR" in line).count()
+warn_count  = rdd.filter(lambda line: "WARN"  in line).count()
+info_count  = rdd.filter(lambda line: "INFO"  in line).count()
+print(f"Nombre de lignes : ERROR = {error_count}, WARN = {warn_count}, INFO = {info_count}\n")
 
+print("Exemple de lignes du log :")
 for line in rdd.take(10):
     print(line)
 
